cd2ef8027138fe7c1c080ed587d48b421a8814a7
Y.C. Liu <yechaoliu1994@outlook.com>
2025-07-30 16:36:17 +0200

    examples and results

---
+++ examples/10qubits.jl
@@ -0,0 +1,70 @@
+# using shortcut mode, we can speed up the computation thus to handle larger systems
+using EntanglementDetection
+using FrankWolfe
+using Ket
+import LinearAlgebra as LA
+using HiGHS
+import MathOptInterface as MOI
+using Random
+using Serialization
+
+output_dir = "EntanglementDetection/examples/results/10qubits"
+
+
+max_length = 10^8
+T = Float64
+
+# Initial the entanglement structure
+N = 10
+dims = Tuple(fill(2, N))
+lmo = EntanglementDetection.AlternatingSeparableLMO(T, dims; nb = 10, threshold = Base.rtoldefault(T), max_iter = 10^3, parallelism = true)
+
+Random.seed!(0)
+println("Initializing the state ...")
+v = 0.3
+ρ = Ket.state_ghz(Complex{T}, 2, N; v = v)
+println("Converting ...")
+C = correlation_tensor(ρ, dims)
+
+println("Beginning the computation ...")
+# Using the Frank-Wolfe algorithm with LP solver
+ini_tensor = similar(C)
+ini_tensor .= 0
+ini_tensor[1] = one(T)
+v0 = FrankWolfe.compute_extreme_point(lmo, ini_tensor - C)
+o = MOI.instantiate(MOI.OptimizerWithAttributes(HiGHS.Optimizer, MOI.Silent() => true))
+active_set = FrankWolfe.ActiveSetQuadraticLinearSolve(
+    FrankWolfe.ActiveSetQuadraticProductCaching([(one(T), copy(v0))], LA.I, -C),
+    LA.I,
+    -C,
+    o,
+    wolfe_step=true
+    )
+
+res = separable_distance(C, lmo;
+    fw_algorithm = FrankWolfe.blended_pairwise_conditional_gradient,
+    verbose = 2,
+    active_set = active_set,
+    max_iteration = 10^3,
+    callback_iter = 2,
+    recompute_last_vertex = false,
+    epsilon = 1e-8,
+    shortcut = true,
+    shortcut_scale = 10,
+    trajectory = true,
+)
+
+abs_output =  string(output_dir , "/ghz_" ,N, ".dat")
+serialize(abs_output, res.x)
+
+traj = res.traj_data
+n_points = 10^3
+indices = round.(Int, range(1, length(traj)-1, length=n_points))
+itr = [traj[i][1] for i in indices]
+primal = [traj[i][2] for i in indices]
+dual_gap = [traj[i][4] for i in indices]
+time = [traj[i][5] for i in indices]
+
+plot_data = (itr, primal, dual_gap, time)
+plot_dir =  string(output_dir , "/ghz_" ,N , "_plot.dat")
+serialize(plot_dir, plot_data)
---
+++ examples/gme.jl
@@ -0,0 +1,72 @@
+# using lmo to define the entanglement structure
+# we supply the ``EntanglementDetection.KSeparableLMO`` for K-separability structure, ``k=2'' corresponding to GME
+# this is example is to achieve the entanglement and separability bound for robustness problem under GME
+using EntanglementDetection
+using FrankWolfe
+using Ket
+using LinearAlgebra
+using HiGHS
+import MathOptInterface as MOI
+using Serialization
+
+max_iteration = 10^7    # control time
+callback_iter = 10^3    
+max_active = 4 * 10^4   # control the size of memory
+shortcut_scale = 10
+noise_atol = 1e-3
+
+
+output_dir = "EntanglementDetection/examples/results/gme"
+
+T = Float64
+N = 4
+dims = Tuple(fill(2, N))
+ρ = Ket.state_ghz(Complex{T}, 2, N)
+
+
+k = 2
+C = correlation_tensor(ρ, dims)
+lmo = EntanglementDetection.KSeparableLMO(T, dims, k; nb = 10, threshold = Base.rtoldefault(T), max_iter = 10^3, parallelism = true)
+
+# the noise_mixture mode can fast reach the boundary of the separable set (outside)
+res = separable_distance(C, lmo;
+    fw_algorithm = FrankWolfe.blended_pairwise_conditional_gradient,
+    verbose = 2,
+    max_iteration = max_iteration,
+    callback_iter = callback_iter,
+    recompute_last_vertex = false,
+    epsilon = 1e-7,
+    noise_mixture = true,
+    noise_atol = noise_atol,
+    shortcut = true,
+    shortcut_scale = shortcut_scale,
+    trajectory = false,
+    max_active = max_active,
+)
+
+ρ_noise(p) = (1-p) * ρ + p * Matrix{Complex{T}}(I, prod(dims), prod(dims)) / prod(dims)
+
+Δp = zero(T)
+sep = (sep = false, res = (active_set = nothing, primal = zero(T)), r = T[], ϵ = T[])
+for p in res.noise_level+noise_atol:noise_atol:1.0
+    println("Testing p = ", p)
+    sep = separability_certification(ρ_noise(p), dims, lmo; 
+        certify_test = 30,
+        max_iteration = max_iteration,
+        epsilon = 1e-7,
+        shortcut = true,
+        trajectory = false,
+        max_active = max_active,
+        )
+    if sep.sep
+        Δp = p - res.noise_level
+        break
+    end
+end
+println(Δp)
+
+txt_output =  string(output_dir, "/results_", k, ".txt")
+open(txt_output, "w") do io
+    p = res.noise_level
+    write(io, "$p\n$Δp")
+end
